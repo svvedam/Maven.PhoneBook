fe849bdb4c80d7ba76e5995e8e3e30515e12ced6
Sravani Vedam <sravanivedam@zipcoders-MBP-4.localdomain>
2019-02-14 19:08:26 -0500

    Completed the Phone Book assignment

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,56 @@
 package com.zipcodewilmington.phonebook;
 
+import java.util.*;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+    String name;
+    String phoneNumber;
+
+    TreeMap<String, List<String>> tmap = new TreeMap<String, List<String>>();
+
+
+    public void add(String name, List<String> phoneNumbers){
+       // adds an entry to the composite associate data type
+        tmap.put(name,phoneNumbers);
+
+    }
+    public void remove(String name){
+        //removes an entry to the composite associate data type
+        tmap.remove(name);
+    }
+    public List<String> lookup(String name){
+        //returns a phone numbers for the respective input name
+        List<String> phoneNumbers = tmap.get(name);
+        return phoneNumbers;
+    }
+    public String reverseLookup(String phoneNumber){
+        String name = "";
+        //returns a name for the respective input phoneNumber
+        for(Map.Entry<String, List<String>> entry : tmap.entrySet()){
+            List<String> str = entry.getValue();
+            for(String s: str){
+                if(s.equals(phoneNumber)){
+                      name = entry.getKey();
+                }
+            }
+        }
+        return name;
+    }
+
+   public void display(){
+        //print a human-readable list of all entries (names and phone numbers) in alphabetical order.
+       Set set = tmap.entrySet();
+       Iterator iterator = set.iterator();
+       while(iterator.hasNext()){
+           Map.Entry mentry = (Map.Entry)iterator.next();
+           System.out.println(mentry.getKey() + " " + mentry.getValue());
+       }
+    }
+
+    public void removeRecord(String name) {
+        tmap.remove(name);
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,133 @@
 package com.zipcodewilmington.phonebook;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+    @Test
+    public void addTest(){
+        //Given
+        String name = "Sravani";
+        List<String> phoneNumbers = new ArrayList<String>();
+        phoneNumbers.add("232-454-6778");
+        phoneNumbers.add("565-545-3434");
+
+        PhoneBook newPhoneBook = new PhoneBook();
+        int count = 0;
+
+        //When
+        newPhoneBook.add(name, phoneNumbers);
+        count++;
+
+        //Then
+        int listSize = newPhoneBook.tmap.size();
+        Assert.assertEquals(count, listSize);
+    }
+    @Test
+    public void removeTest(){
+        //Given
+        String name = "Sravani";
+        List<String> phoneNumbers = new ArrayList<String>();
+        phoneNumbers.add("232-454-6778");
+        phoneNumbers.add("565-545-3434");
+        int count = 1;
+        PhoneBook newPhoneBook = new PhoneBook();
+        newPhoneBook.add(name, phoneNumbers);
+
+
+        //When
+        newPhoneBook.remove("Sravani");
+        count--;
+
+        //Then
+        int listSize = newPhoneBook.tmap.size();
+        Assert.assertEquals(count, listSize);
+    }
+    @Test
+    public void lookupTest(){
+        String name = "Sravani";
+        List<String> phoneNumbers = new ArrayList<String>();
+        List<String> retrievedPhoneNumbers = new ArrayList<String>();
+        phoneNumbers.add("232-454-6778");
+        phoneNumbers.add("565-545-3434");
+        PhoneBook newPhoneBook = new PhoneBook();
+        newPhoneBook.add(name, phoneNumbers);
+
+        //When
+        retrievedPhoneNumbers = newPhoneBook.lookup("Sravani");
+
+        //Then
+        Assert.assertEquals(phoneNumbers, retrievedPhoneNumbers);
+    }
+    @Test
+    public void reverseLookupTest(){
+        //Given
+        String name = "Sravani";
+        List<String> phoneNumbers = new ArrayList<String>();
+
+        phoneNumbers.add("232-454-6778");
+        phoneNumbers.add("565-545-3434");
+        PhoneBook newPhoneBook = new PhoneBook();
+        newPhoneBook.add(name, phoneNumbers);
+
+        //When
+        String retrievedName = newPhoneBook.reverseLookup("232-454-6778");
+
+        //Then
+        Assert.assertEquals(name, retrievedName);
+    }
+    @Before
+    public void setUpStreams(){
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+    @Test
+    public void displayTest(){
+        //Given
+        String name = "Sravani";
+        List<String> phoneNumbers = new ArrayList<String>();
+
+        phoneNumbers.add("232-454-6778");
+        phoneNumbers.add("565-545-3434");
+        PhoneBook newPhoneBook = new PhoneBook();
+        newPhoneBook.add(name, phoneNumbers);
+
+        //When
+        newPhoneBook.display();
+
+        //Then
+        Assert.assertEquals("Sravani [232-454-6778, 565-545-3434]", outContent.toString().trim());
+    }
+    @Test
+    public void removeRecordTest(){
+        //Given
+        String name = "Sravani";
+        List<String> phoneNumbers = new ArrayList<String>();
+
+        phoneNumbers.add("232-454-6778");
+        phoneNumbers.add("565-545-3434");
+        PhoneBook newPhoneBook = new PhoneBook();
+        newPhoneBook.add(name, phoneNumbers);
+        int count = 1;
+
+        //When
+        newPhoneBook.removeRecord(name);
+        count--;
+
+        //Then
+        int listSize = newPhoneBook.tmap.size();
+        Assert.assertEquals(count, listSize);
+    }
 }
